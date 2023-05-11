e43fc8e315ccc3338e992fd89d25174432506c7b
Malikova Anastasia <ansty.mlkv@icloud.com>
2023-05-12 00:17:10 +0500

    git@github.com:anstymlkv/how-to-learn.git

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// объявляем переменную для хранения счётчика километров
+let kmCounter = document.querySelector('#counter-km');
+kmCounter = 122;
+console.log(kmCounter)
+// объявляем переменную для хранения счётчика дней
+let dayCounter = document.querySelector('#counter-days');
